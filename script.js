9a69b92c9db904797c84bb2e72c8b6e413037f25
gregdu91 <greg91400@hotmail.fr>
2019-11-24 22:03:15 +0100

    2

--- script.js
+++ script.js
@@ -1,25 +1,128 @@
-// Get the modal
-var modal = document.getElementById("myModal");
+let ordre = [];
+let joueurTour = [];
+let flash;
+let turn;
+let PcTour;
+let son = true;
+let intervalId;
+let bon;
+let win;
 
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+const score = document.querySelector("#score");
+const csRed = document.querySelector("#red");
+const csBlue = document.querySelector("#blue");
+const csGreen = document.querySelector("#green");
+const csYellow = document.querySelector("#yellow");
 
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
-}
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
+function play() {
+  win = false;
+  ordre = [];
+  joueurTour = [];
+  flash = 0;
+  intervalId = 0;
+  turn = 1;
+  for (var i = 0; i < 20; i++) {
+    ordre.push(Math.floor(Math.random() * 4) + 1);
   }
+
+  PcTour = true;
+
+  intervalId = setInterval(simonTour, 1000)
+
 }
+
+function simonTour() {
+  if (flash == turn) {
+    clearInterval(intervalId)
+    PcTour =false;
+    clearColor();
+  }
+
+    if (PcTour) {
+      clearColor();
+      setTimeout(() => {
+        if (ordre[flash] == 1) red();
+        if (ordre[flash] == 2) blue();
+        if (ordre[flash] == 3) green();
+        if (ordre[flash] == 4) yellow();
+        flash++;
+      }, 200);
+
+    }
+}
+
+
+function clearColor() {
+  csRed.style.backgroundColor = "red";
+  csBlue.style.backgroundColor = "blue";
+  csGreen.style.backgroundColor = "green";
+  csYellow.style.backgroundColor = "yellow";
+}
+
+function red() {
+  if (son) {
+
+    let audio = document.getElementById("do");
+    audio.play();
+  }
+
+  son = true;
+  csRed.style.backgroundColor = "tomato";
+}
+
+function blue() {
+  if (son) {
+
+    let audio = document.getElementById("re");
+    audio.play();
+  }
+
+  son = true;
+  csBlue.style.backgroundColor = "lightskyblue";
+}
+
+function green() {
+  if (son) {
+
+    let audio = document.getElementById("mi");
+    audio.play();
+  }
+
+  son = true;
+  csGreen.style.backgroundColor = "lightgreen";
+}
+
+function yellow() {
+  if (son) {
+
+    let audio = document.getElementById("fa");
+    audio.play();
+  }
+
+  son = true;
+  csYellow.style.backgroundColor = "white";
+}
+
+csRed.addEventListener('click', (event) => {
+  if (win) {
+    joueurTour.push(1);
+    //check();
+    red();
+  }
+  if (!win) {
+    setTimeout(() => {
+      clearColor;
+
+    },300    
+      )
+  }
+  })
+
+
+
+
+
+
+
---
+++ test.js
@@ -0,0 +1,12 @@
+let order = [];
+let PlayerOrder =[];
+let flash[];
+let turn;
+let good;
+let compTurn;
+let stric = false;
+let noise = false;
+let on = true;
+let win;
+
+const turCounter = document.ElementById("")
